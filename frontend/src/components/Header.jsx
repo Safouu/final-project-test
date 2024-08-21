22e1e357d2466b93eaa7c165bad2c03edbc99857
Javaid Afsoon <Javaid.afsoon@dci-student.org>
2024-08-21 11:03:29 +0200

    Logout login Handling Added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,35 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 const Header = () => {
-    return (
-        <header className="header">
-            <NavLink to="/" className="nav-link">Home</NavLink>
-            <NavLink to="/contact" className="nav-link">Contact</NavLink>
-            <NavLink to="/login" className="nav-link">Login</NavLink>  
-            <NavLink to="/logout" className= "nav-link">Logout</NavLink> 
-        </header>
-    ) 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    
+    const isAdmin = localStorage.getItem('isAdmin');
+    if (isAdmin) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    
+    localStorage.removeItem('isAdmin');
+    localStorage.removeItem('userToken');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <header className="header">
+      <NavLink to="/" className="nav-link">Home</NavLink>
+      <NavLink to="/contact" className="nav-link">Contact</NavLink>
+      {isLoggedIn ? (
+        <NavLink to="/login" className="nav-link" onClick={handleLogout}>Logout</NavLink>
+      ) : (
+        <NavLink to="/login" className="nav-link">Login</NavLink>
+      )}
+    </header>
+  );
 };
-  
-  export default Header;
+
+export default Header;
