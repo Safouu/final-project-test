745cd8a0465ac6ca5263f14cb8aa57f88057f24b
Javaid Afsoon <Javaid.afsoon@dci-student.org>
2024-09-13 14:41:35 +0200

    Prices array is changed by price

--- backend/controllers/objectController.js
+++ backend/controllers/objectController.js
@@ -15,8 +15,8 @@ export const getObjects = async (req, res) => {
   export const postObject = async (req, res) => {
     try {
       await connect();
-      const { name, prices, description, image,image1, image2, image3, image4, image5, image6, latitude, longitude  } = req.body;
-      const newObject = new Object({ name, prices, description, image, image1, image2, image3, image4, image5, image6, latitude, longitude });
+      const { name, price, description, image,image1, image2, image3, image4, image5, image6, latitude, longitude  } = req.body;
+      const newObject = new Object({ name, price, description, image, image1, image2, image3, image4, image5, image6, latitude, longitude });
       await newObject.save();
       res.status(201).json(newObject);
     } catch (e) {
